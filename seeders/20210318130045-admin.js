d34417af84d78b5b62957bc8ed305f1ace43f439
chejg7 <chejg7@gmail.com>
2021-03-18 23:58:57 +0900

    add controllers

---
+++ seeders/20210318130045-admin.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Admins', [{
+      email: 'chejg7@gmail.com',
+      name: 'jk',
+      password: 'cmoe2021',
+      createdAt: new Date,
+      updatedAt: new Date
+    }]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    await queryInterface.bulkDelete('Admins', null, {});
+  }
+};
