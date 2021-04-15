4c1f998aeb7962390b213cf5a50dc1a1ac2c5311
chejg7 <chejg7@gmail.com>
2021-04-16 00:09:31 +0900

    fix config

--- config/config.js
+++ config/config.js
@@ -1,9 +1,12 @@
-{
+require('dotenv').config()
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
+    "username": process.env.MY_DB_USERNAME,
+    "password": process.env.MY_DB_PASSWORD,
+    "database": process.env.MY_DB_DBNAME,
+    "host": process.env.MY_DB_HOST,
+    "port": process.env.MY_DB_PORT,
     "dialect": "mysql"
   },
   "test": {
